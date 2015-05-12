16f8f7bf03519ac3c1c0bb1ad5f89cd8b2874afc
javajigi <javajigi@gmail.com>
2015-05-12 22:03:05 +0900

    컴퓨터 숫자를 shuffle로 구현함 for문 학습함

--- src/Park.java
+++ src/Park.java
@@ -1,5 +1,19 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 
 public class Park {
+	static int[] com () {
+		List<Integer> nos = Arrays.asList(0,1,2,3,4,5,6,7,8,9);
+		Collections.shuffle(nos);
+		int[] j = new int[3];
+		j[0] = nos.get(0); 
+		j[1] = nos.get(1); 
+		j[2] = nos.get(2); 
+		return j;
+	}
+	
 	static int sum(int[] v) {
 		int p = 0;
 		int k = 0;
@@ -16,33 +30,28 @@ public class Park {
 	}
 	
 	static int strike(int[] com, int[] my) {
-		int j = 0;
 		int s = 0;
-		while(j < com.length){
+		for(int j = 0; j < com.length; j++){
 			int c = com[j];
 			int m = my[j];
 			if(c == m ){
 				s++;
 			}
-			j++; 
 		}
 		return s;
 	}
 	
 	static int ball(int[] com, int[] my) {
-		int j = 0;
 		int b = 0;
-		while(j < com.length){
+		for(int j = 0; j < com.length; j++){
 			int c = com[j];
-			int l = 0;
-			while(l < my.length){
+			for(int l = 0; l < my.length; l++){
 				int m = my[l];
 				if(c == m && j != l){
+					
 					b++;
 				}
-				l++;
 			}
-			j++;
 		}
 		
 		return b;
@@ -53,5 +62,18 @@ public class Park {
 		int[] my = {2,3,5};
 		System.out.println("Strike : " + strike(com, my));
 		System.out.println("Ball : " + ball(com, my));
+		int[] j = com();
+		for(int h = 0; h < j.length; h++) {
+			int n = j[h];
+			System.out.println(" 컴퓨터가 썩은 값 " + n );
+		}
+		
+//		int h = 0;
+//		while(h < j.length ){
+//			int n = j[h];
+//			System.out.println(" 컴퓨터가 썩은 값 " + n );
+//			h++;
+//		}
+		
 	}
 }
